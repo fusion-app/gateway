a2a3e965aac7d4fb6be06b5141c3d64e7ca52ff6
menyakun <lxs137@hotmail.com>
2021-04-26 00:38:11 +0800

    init

---
+++ Makefile
@@ -0,0 +1,67 @@
+VERSION ?= 0.0.1
+
+TAG = $(shell date -u +"%Y%m%d%H%M")
+IMG ?= registry.cn-shanghai.aliyuncs.com/fusion-app/gateway:$(TAG)
+
+# Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
+ifeq (,$(shell go env GOBIN))
+GOBIN=$(shell go env GOPATH)/bin
+else
+GOBIN=$(shell go env GOBIN)
+endif
+
+all: build
+
+##@ General
+
+# The help target prints out all targets with their descriptions organized
+# beneath their categories. The categories are represented by '##@' and the
+# target descriptions by '##'. The awk commands is responsible for reading the
+# entire set of makefiles included in this invocation, looking for lines of the
+# file as xyz: ## something, and then pretty-format the target and help. Then,
+# if there's a line with ##@ something, that gets pretty-printed as a category.
+# More info on the usage of ANSI control characters for terminal formatting:
+# https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_parameters
+# More info on the awk command:
+# http://linuxcommand.org/lc3_adv_awk.php
+
+help: ## Display this help.
+	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[a-zA-Z_0-9-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
+
+##@ Development
+
+fmt: ## Run go fmt against code.
+	go fmt ./...
+
+vet: ## Run go vet against code.
+	go vet ./...
+
+##@ Build
+
+build: generate fmt vet ## Build manager binary.
+	go build -o bin/manager main.go
+
+run: manifests generate fmt vet ## Run a controller from your host.
+	go run ./main.go
+
+docker-build: test ## Build docker image with the manager.
+	docker build -t ${IMG} .
+
+docker-push: ## Push docker image with the manager.
+	docker push ${IMG}
+
+
+# go-get-tool will 'go get' any package $2 and install it to $1.
+PROJECT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
+define go-get-tool
+@[ -f $(1) ] || { \
+set -e ;\
+TMP_DIR=$$(mktemp -d) ;\
+cd $$TMP_DIR ;\
+go mod init tmp ;\
+echo "Downloading $(2)" ;\
+GOBIN=$(PROJECT_DIR)/bin go get $(2) ;\
+rm -rf $$TMP_DIR ;\
+}
+endef
+
---
+++ api/v1alpha1/resourcemonitor_types.go
@@ -0,0 +1,95 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ResourceMonitorSpec defines the desired state of ResourceMonitor
+type ResourceMonitorSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of ResourceMonitor. Edit resourcemonitor_types.go to remove/update
+	Selector     SelectorSpec     `json:"selector"`
+	ChangeFilter ChangeFilterSpec `json:"changeFilter"`
+	BackendSpec  `json:",inline"`
+}
+
+type SelectorSpec struct {
+	Namespace  string            `json:"namespace,omitempty"`
+	Label      map[string]string `json:"label,omitempty"`
+	Annotation map[string]string `json:"annotation,omitempty"`
+}
+
+type ChangeFilterSpec struct {
+	Type ChangeFilterType `json:"type"`
+}
+
+type ChangeFilterType string
+
+const (
+	JSONDiff ChangeFilterType = "JSONDiff"
+)
+
+type BackendSpec struct {
+	MQTTBackend *MQTTBackendSpec `json:"mqttBackend,omitempty"`
+}
+
+type MQTTBackendSpec struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	Topic    string `json:"topic"`
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+// ResourceMonitorStatus defines the observed state of ResourceMonitor
+type ResourceMonitorStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Selected int `json:"selected,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// ResourceMonitor is the Schema for the resourcemonitors API
+type ResourceMonitor struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ResourceMonitorSpec   `json:"spec,omitempty"`
+	Status ResourceMonitorStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ResourceMonitorList contains a list of ResourceMonitor
+type ResourceMonitorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ResourceMonitor `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ResourceMonitor{}, &ResourceMonitorList{})
+}
